3fcf356470543140e4fb462fd5e0f2de77933115
Oleksiy <oleksii.oliinyk@asideas.de>
2022-05-02 13:44:30 +0200

    added phonemizing user input

--- utils.py
+++ utils.py
@@ -1,8 +1,8 @@
 from typing import List
 import re
+from espeak_phonemizer import EspeakPhonemizer
 
-
-def search(transcription: str, query: str) -> List:
+def search(transcription: str, query: str, language:str = "en") -> List:
     """
     Search for the substring and return positions
     :parameter transcription a list of tuples with the character and start time, eg [('a', 0.2), ('b', 0.4)]
@@ -19,8 +19,12 @@ def search(transcription: str, query: str) -> List:
     # clean user input
     cleaned_query = "".join([x for x in query.lower() if x.isalnum()])
 
+    # phonemize user query
+    espeak_phonemizer = EspeakPhonemizer()
+    phonemized_input = espeak_phonemizer(cleaned_query, language=language)
+
     # do search
-    found_durations = [(m.start(0), m.end(0)) for m in re.finditer(cleaned_query, reconstructed_sample)]
+    found_durations = [(m.start(0), m.end(0)) for m in re.finditer(phonemized_input, reconstructed_sample)]
 
     for duration in found_durations:
         start_pos, end_pos = duration
@@ -28,3 +32,11 @@ def search(transcription: str, query: str) -> List:
         result_durations.append((durations[0], durations[-1]))
 
     return result_durations
+
+if __name__ == '__main__':
+    sample_phonemized = [("h", 0.1), ("ə", 0.3), ("l", 0.5), ("ʊ", 0.7), (" ", 0.8),
+                         ("w", 0.3), ("ɜː", 0.5), ("l", 0.7), ("d", 0.8)]
+    query = "world"
+
+    found_durations = search(sample_phonemized, query)
+    print(found_durations)
