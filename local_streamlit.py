d741cbaa847c37623b07fe2ead20c3cc9ad8f30a
Tanuj Jain <tanuj.jain.10@gmail.com>
2022-05-02 13:12:16 +0200

    Added search mocks.

--- local_streamlit.py
+++ local_streamlit.py
@@ -1,19 +1,29 @@
 import streamlit as st
 
 
+def mocksearch_service(text):
+    print(f'Searching for {text}')
+    return [(1, 3), (68, 76)]
+
+
+def process_timestamps(ts_list):
+    print('Getting ready to display your search results ..')
+
+
 def audio(filename):
     audio_file = open(filename, 'rb')
     audio_bytes = audio_file.read()
     st.audio(audio_bytes, format='audio/ogg')
 
 
-
 def local_css(file_name):
     with open(file_name) as f:
         st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
+
 def remote_css(url):
     st.markdown(f'<link href="{url}" rel="stylesheet">', unsafe_allow_html=True)
+
 
 def icon(icon_name):
     st.markdown(f'<i class="material-icons">{icon_name}</i>', unsafe_allow_html=True)
@@ -25,8 +35,9 @@ if __name__ == '__main__':
 
     icon("search")
     selected = st.text_input("", "Search...")  # searched text goes into this var
-    print(selected)
-    button_clicked = st.button("OK") ## Bool Flag
-    print(button_clicked)
+    button_clicked = st.button("OK")  # Bool Flag, indicates whether 'ok' button has been clicked
+    if button_clicked:
+        timestamp_list = mocksearch_service(selected)
+        process_timestamps(timestamp_list)
 
     audio('/Users/tjain1/Downloads/STU1 MOD R rough.wav')
