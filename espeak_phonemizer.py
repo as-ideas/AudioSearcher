97aef6bd059748c8e247635cc7174d5b0900eae5
cschafer26 <c.schaefer.home@gmail.com>
2022-05-02 13:00:48 +0200

    Add espeak phonemizer.

---
+++ espeak_phonemizer.py
@@ -0,0 +1,20 @@
+from phonemizer.phonemize import phonemize
+
+
+class EspeakPhonemizer:
+
+    def __call__(self, text: str, language: str) -> str:
+        return phonemize(text,
+                         language=language,
+                         backend='espeak',
+                         strip=True,
+                         preserve_punctuation=True,
+                         with_stress=False,
+                         njobs=1,
+                         language_switch='remove-flags')
+
+
+if __name__ == '__main__':
+    espeak_phonemizer = EspeakPhonemizer()
+    phones = espeak_phonemizer('Tanuj und Oleksi sind schlaue Säcke!', language='de')
+    print(phones)
