308ea94e428aab8250ac53b38ec4668d801de844
cschafer26 <c.schaefer.home@gmail.com>
2022-05-03 12:03:28 +0200

    Improve local streamlit.

--- local_streamlit.py
+++ local_streamlit.py
@@ -47,15 +47,24 @@ def remote_css(url):
 def icon(icon_name):
     st.markdown(f'<i class="material-icons">{icon_name}</i>', unsafe_allow_html=True)
 
+@st.cache
+def get_audio_transcription(file):
+    audio_input, sample_rate = librosa.load(file, sr=16000)
+    transcriber = Transcriber()
+    transcribed = transcriber(audio_input)
+    return audio_input, transcribed
+
 
 wav_file = '/Users/cschaefe/datasets/bild_snippets_cleaned/Snippets/r_0367_046-r_0367_047.wav'
-audio_input, sample_rate = librosa.load(wav_file, sr=16000)
-espeak_phonemizer = EspeakPhonemizer()
-transcriber = Transcriber()
-searcher = Searcher()
-transcribed = transcriber(audio_input)
-audio(wav_file)
 
+
+audio_input, transcribed = get_audio_transcription(wav_file)
+espeak_phonemizer = EspeakPhonemizer()
+searcher = Searcher()
+audio(wav_file)
+reconstructed_sample = [x[0] for x in transcribed]
+reconstructed_sample = ''.join(reconstructed_sample).lower()
+st.write(reconstructed_sample)
 
 if __name__ == '__main__':
     local_css("style.css")
@@ -63,19 +72,22 @@ if __name__ == '__main__':
 
     icon("search")
     query = st.text_input("", "Search...")  # searched text goes into this var
-    button_clicked = st.button("OK")  # Bool Flag, indicates whether 'ok' button has been clicked
+    button_de = st.button("DE")  # Bool Flag, indicates whether 'ok' button has been clicked
+    button_en = st.button("EN")  # Bool Flag, indicates whether 'ok' button has been clicked
 
-    if button_clicked:
-        phonemized_query = espeak_phonemizer(query, language='de')
-        st.write(f'Looking for phonemes: {phonemized_query}')
+
+    if button_de or button_en:
+        language = 'de' if button_de else 'en'
+        phonemized_query = espeak_phonemizer(query, language='en')
+        st.write(f'Looking for query: {query}, language: {language}, phonemes: {phonemized_query}')
         phonemized_query = phonemized_query.replace(' ', '')
 
         reconstructed_sample = [x[0] for x in transcribed]
         reconstructed_sample = ''.join(reconstructed_sample).lower()
         print(reconstructed_sample)
 
-        timestamp_list = searcher(transcribed, phonemized_query, language='de', max_char_errors=int(len(query) * 0.3))
-        timestamp_list.sort(key=lambda x: x[-1])
+        timestamp_list = searcher(transcribed, phonemized_query, language='de', max_char_errors=int(len(query) * 0.5))
+        timestamp_list.sort(key=lambda x: -x[-1])
         print(timestamp_list)
         if len(timestamp_list) == 0:
             st.write(f'Could not find what you were looking for...')
